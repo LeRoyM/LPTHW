25371837d97a8d1a71088081128ea1d3eeb23b5a
LeRoyM <github.333@leroyhome.com>
2018-06-27 09:47:33 -0400

    testing branch edit

--- ex19.py
+++ ex19.py
@@ -1,3 +1,4 @@
+#will this edit be seen in new branch
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print "You have %d cheeses!" % cheese_count
     print "You have %d boxes of crackers!" % boxes_of_crackers
